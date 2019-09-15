18187976bbfdf0dbe2e36ddf8a5268304755a751
Erin <erin.nienhuis@gmail.com>
2019-09-15 15:15:04 -0400

    Updated server.js to return documentation.html if the url contains Documentation

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
-const http = require('http');
+const http = require('http'),
+  fs = require('fs'),
+  url = require('url');
 
 http.createServer((request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Node!\n');
-}).listen(8080);
+  var addr = request.url,
+    q = url.parse(addr,true),
+    filePath = '';
 
-console.log('My first Node test server is running on Port 8080.');
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', function(err) {
+    if (err) {
+      console.log(err);
+    }
+      else {
+        console.log('Added to log.');
+      }
+  });
+
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
+
+  fs.readFile(filePath, function(err, data) {
+    if (err) {
+      throw err;
+    }
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.write(data);
+    response.end();
+  });
+
+}).listen(8080);
