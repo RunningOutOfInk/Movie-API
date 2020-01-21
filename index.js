96ef472b15aa162f12879317180dee68f2deb21d
Erin <erin.nienhuis@gmail.com>
2020-01-20 19:47:27 -0500

    Attempt3 -  query to pull back Director infor from Directors table using a join

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ app.get('/movies', function(req, res) {
   Movies.aggregate([
     { $lookup:
     {
-      from: 'Directors',
+      from: 'directors',
       localField: 'Director',
       foreignField: '_id',
       as: 'moviedirectordetails'
